4e4ec44649344abab7210443962fb6ed9af46ff0
NeAlfa <sharipovdevelopper@gmail.com>
2023-05-03 23:44:01 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -26,3 +26,15 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "../assets/images/light.svg";
+  } else {
+    this.firstElementChild.src = "../assets/images/dark.svg";
+  }
+  document.body.classList.toggle("light");
+});
